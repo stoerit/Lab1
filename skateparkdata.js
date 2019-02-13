24c54ae5de08240cc350f6eb61eb34010b43c397
Stober <stoerit@dunwoody.edu>
2019-02-13 16:12:56 -0600

    adding skateparkdata.js which contains the data about 25 skateparks in MN

---
+++ skateparkdata.js
@@ -0,0 +1,252 @@
+var skateparks = [
+  {
+    "name": "3rd Lair",
+    "latitude": 44.97302,
+    "longtitude": -93.36363,
+    "city": "Golden Valley",
+    "state": "Minnesota",
+    "address": "850 Florida Ave S",
+    "zip": 55426,
+    "type": "indoor"
+  },
+  {
+    "name": "Overpass",
+    "latitude": 44.91887,
+    "longtitude": -93.4012,
+    "city": "Hopkins",
+    "state": "Minnesota",
+    "address": "100 Washington Ave S",
+    "zip": 55343,
+    "type": "outdoor"
+  },
+  {
+    "name": "St Louis Park",
+    "latitude": 44.94514,
+    "longtitude": -93.34203,
+    "city": "St Louis Park",
+    "state": "Minnesota",
+    "address": "4900 MN-7",
+    "zip": 55416,
+    "type": "ourdoor"
+  },
+  {
+    "name": "Familia",
+    "latitude": 44.99191,
+    "longtitude": -93.24485,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "address": "835 E Hennepin Ave",
+    "zip": 55414,
+    "type": "indoor"
+  },
+  {
+    "name": "Maple Grove",
+    "latitude": 45.10302,
+    "longtitude": -93.44414,
+    "city": "Maple Grove",
+    "state": "Minnesota",
+    "address": "12951 Weaver Lake Rd",
+    "zip": 55369,
+    "type": "outdoor"
+  },
+  {
+    "name": "Burnsville",
+    "latitude": 44.76559,
+    "longtitude": -93.27398,
+    "city": "Burnsville",
+    "state": "Minnesota",
+    "address": "151 Civic Center Pkwy",
+    "zip": 55337,
+    "type": "outdoor"
+  },
+  {
+    "name": "Help Boardshop",
+    "latitude": 44.86871,
+    "longtitude": -93.35913,
+    "city": "Edina",
+    "state": "Minnesota",
+    "address": "7385 Bush Lake Rd",
+    "zip": 55439,
+    "type": "indoor"
+  },
+  {
+    "name": "Front Skatepark",
+    "latitude": 44.9693,
+    "longtitude": -93.1161,
+    "city": "St Paul",
+    "state": "Minnesota",
+    "address": "485 Stinson Ave W",
+    "zip": 55117,
+    "type": "outdoor"
+  },
+  {
+    "name": "Zero Gravity",
+    "latitude": 44.93528,
+    "longtitude": -93.66034,
+    "city": "Mound",
+    "state": "Minnesota",
+    "address": "2345 Cypress Ln",
+    "zip": 55364,
+    "type": "outdoor"
+  },
+  {
+    "name": "Bemidji Skatepark",
+    "latitude": 47.490291,
+    "longtitude": -94.899988,
+    "city": "Bemidji",
+    "state": "Minnesota",
+    "address": "1504 23rd St NW",
+    "zip": 56601,
+    "type": "outdoor"
+  },
+  {
+    "name": "Augsburg Park",
+    "latitude": 44.968792,
+    "longtitude": -93.277817,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "address": "7000 Nicollet Ave",
+    "zip": 55423,
+    "type": "outdoor"
+  },
+  {
+    "name": "Morris Park",
+    "latitude": 44.90322,
+    "longtitude": -93.216409,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "address": "5531 39th Ave S",
+    "zip": 55417,
+    "type": "outdoor"
+  },
+  {
+    "name": "Glen Lake Skatepark",
+    "latitude": 44.906384,
+    "longtitude": -93.461678,
+    "city": "Minnetonka",
+    "state": "Minnesota",
+    "address": "14350 Excelsior Blvd",
+    "zip": 55345,
+    "type": "outdoor"
+  },
+  {
+    "name": "Elliot Skatepark",
+    "latitude": 44.968999,
+    "longtitude": -93.259464,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "address": "1000 E 14th St",
+    "zip": 55404,
+    "type": "outdoor"
+  },
+  {
+    "name": "Brooklyn Park Skatepark",
+    "latitude": 44.96313,
+    "longtitude": -93.266563,
+    "city": "Brooklyn Park",
+    "state": "Minnesota",
+    "address": "5700 85th Ave N",
+    "zip": 55443,
+    "type": "outdoor"
+  },
+  {
+    "name": "Shoreview Skatepark",
+    "latitude": 45.080468,
+    "longtitude": -93.131686,
+    "city": "Shoreview",
+    "state": "Minnesota",
+    "address": "4580 Victoria St N",
+    "zip": 55126,
+    "type": "outdoor"
+  },
+  {
+    "name": "Albertville Skatepark",
+    "latitude": 45.23526,
+    "longtitude": -93.655215,
+    "city": "Albertville",
+    "state": "Minnesota",
+    "address": "5798 Lander Ave NE",
+    "zip": 55301,
+    "type": "outdoor"
+  },
+  {
+    "name": "Lexington-Diffley",
+    "latitude": 44.804491,
+    "longtitude": -93.14379,
+    "city": "Eagan",
+    "state": "Minnesota",
+    "address": "4201 Lexington Ave S",
+    "zip": 55123,
+    "type": "outdoor"
+  },
+  {
+    "name": "Brackett Skatepark",
+    "latitude": 44.952521,
+    "longtitude": -93.254083,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "address": "2728 S 39th Ave",
+    "zip": 55406,
+    "type": "outdoor"
+  },
+  {
+    "name": "Champlin Skatepark",
+    "latitude": 45.160766,
+    "longtitude": -93.357397,
+    "city": "Champlin",
+    "state": "Minnesota",
+    "address": "8001-7901 114th Ave N",
+    "zip": 55316,
+    "type": "outdoor"
+  },
+  {
+    "name": "Chaska Skatepark",
+    "latitude": 44.805942,
+    "longtitude": -93.596872,
+    "city": "Chaska",
+    "state": "Minnesota",
+    "address": "1661 Park Ridge Dr",
+    "zip": 55318,
+    "type": "outdoor"
+  },
+  {
+    "name": "Pine Village Skatepark",
+    "latitude": 45.5571,
+    "longtitude": -93.230193,
+    "city": "Cambridge",
+    "state": "Minnesota",
+    "address": "635 18th Ave SW",
+    "zip": 55008,
+    "type": "outdoor"
+  },
+  {
+    "name": "Stillwater Skatepark",
+    "latitude": 45.037431,
+    "longtitude": -92.846345,
+    "city": "Stillwater",
+    "state": "Minnesota",
+    "address": "2222-2266 Curve Crest Blvd",
+    "zip": 55082,
+    "type": "outdoor"
+  },
+  {
+    "name": "Aitkin Skatepark",
+    "latitude": 46.529418,
+    "longtitude": -93.709773,
+    "city": "Aitkin",
+    "state": "Minnesota",
+    "address": "2nd Ave SW And Potter Blvd",
+    "zip": 56431,
+    "type": "outdoor"
+  },
+  {
+    "name": "Andover Skatepark",
+    "latitude": 45.244484,
+    "longtitude": -93.311195,
+    "city": "Andover",
+    "state": "Minnesota",
+    "address": "1900 Crosstown Blvd NW",
+    "zip": 55304,
+    "type": "outdoor"
+  }
+]
