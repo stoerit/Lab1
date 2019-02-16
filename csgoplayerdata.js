20228e981b60bc61e495a449d436394f6531b6cc
Stober <stoerit@dunwoody.edu>
2019-02-15 18:50:29 -0600

    replacing js file with new one

---
+++ csgoplayerdata.js
@@ -0,0 +1,177 @@
+var players = [
+    {
+      "name": "s1mple",
+      "team": "Natus Vincere",
+      "maps": 191,
+      "k/d": 1.48,
+      "rating": 1.34
+    },
+    {
+      "name": "device",
+      "team": "Astralis",
+      "maps": 234,
+      "k/d": 1.33,
+      "rating": 1.24
+    },
+    {
+      "name": "NiKo",
+      "team": "FaZe",
+      "maps": 216,
+      "k/d": 1.22,
+      "rating": 1.2
+    },
+    {
+      "name": "woxic",
+      "team": "HellRaisers",
+      "maps": 158,
+      "k/d": 1.21,
+      "rating": 1.17
+    },
+    {
+      "name": "electronic",
+      "team": "Natus Vincere",
+      "maps": 187,
+      "k/d": 1.17,
+      "rating": 1.17
+    },
+    {
+      "name": "Magisk",
+      "team": "Astralis",
+      "maps": 234,
+      "k/d": 1.18,
+      "rating": 1.16
+    },
+    {
+      "name": "dupreeh",
+      "team": "Astralis",
+      "maps": 234,
+      "k/d": 1.16,
+      "rating": 1.16
+    },
+    {
+      "name": "NAF",
+      "team": "Team Liquid",
+      "maps": 199,
+      "k/d": 1.13,
+      "rating": 1.15
+    },
+    {
+      "name": "valde",
+      "team": "North",
+      "maps": 208,
+      "k/d": 1.15,
+      "rating": 1.14
+    },
+    {
+      "name": "Twistzz",
+      "team": "Team Liquid",
+      "maps": 193,
+      "k/d": 1.17,
+      "rating": 1.13
+    },
+    {
+      "name": "gla1ve",
+      "team": "Astralis",
+      "maps": 234,
+      "k/d": 1.12,
+      "rating": 1.13
+    },
+    {
+      "name": "Xyp9x",
+      "team": "Astralis",
+      "maps": 234,
+      "k/d": 1.18,
+      "rating": 1.13
+    },
+    {
+      "name": "oskar",
+      "team": "mousesports",
+      "maps": 220,
+      "k/d": 1.13,
+      "rating": 1.11
+    },
+    {
+      "name": "suNny",
+      "team": "mousesports",
+      "maps": 229,
+      "k/d": 1.07,
+      "rating": 1.1
+    },
+    {
+      "name": "ISSAA",
+      "team": "HellRaisers",
+      "maps": 164,
+      "k/d": 1.1,
+      "rating": 1.1
+    },
+    {
+      "name": "EliGE",
+      "team": "Team Liquid",
+      "maps": 199,
+      "k/d": 1.03,
+      "rating": 1.09
+    },
+    {
+      "name": "Kjaerbye",
+      "team": "North",
+      "maps": 208,
+      "k/d": 1.05,
+      "rating": 1.09
+    },
+    {
+      "name": "kennyS",
+      "team": "G2",
+      "maps": 162,
+      "k/d": 1.1,
+      "rating": 1.08
+    },
+    {
+      "name": "KRiMZ",
+      "team": "Fnatic",
+      "maps": 165,
+      "k/d": 1.05,
+      "rating": 1.08
+    },
+    {
+      "name": "ropz",
+      "team": "mousesports",
+      "maps": 229,
+      "k/d": 1.12,
+      "rating": 1.08
+    },
+    {
+      "name": "GuardiaN",
+      "team": "FaZe",
+      "maps": 216,
+      "k/d": 1.12,
+      "rating": 1.08
+    },
+    {
+      "name": "mertz",
+      "team": "North",
+      "maps": 174,
+      "k/d": 1.06,
+      "rating": 1.07
+    },
+    {
+      "name": "REZ",
+      "team": "Ninjas In Pyjamas",
+      "maps": 172,
+      "k/d": 1.01,
+      "rating": 1.06
+    },
+    {
+      "name": "f0rest",
+      "team": "Ninjas In Pyjamas",
+      "maps": 172,
+      "k/d": 1.06,
+      "rating": 1.06
+    },
+    {
+      "name": "flamie",
+      "team": "Natus Vincere",
+      "maps": 189,
+      "k/d": 1.02,
+      "rating": 1.05
+    }
+  ]
